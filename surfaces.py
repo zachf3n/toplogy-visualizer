29d459767697813b22e9d418f19ac32dc9dfc0ed
zachf3n <154467217+zachf3n@users.noreply.github.com>
2026-02-07 14:05:39 -0800

    Checking memory of committing from VS Code.

--- surfaces.py
+++ surfaces.py
@@ -1,1 +1,13 @@
+### Using parametric equations to generate surfaces.
+### Equations for a torus:
+### x(u, v) = (R + r * cos(v)) * cos(u)
+### y(u, v) = (R + r * cos(v)) * sin(u)
+### z(u, v) = r * sin(v)
+### where u, v \in [0, 2 * \pi]
 
+import numpy as np 
+
+
+def torus(R=2.0, r=1.0, u_res=40, v_res=20):
+    """Parametric equations for torus to generate mesh"""
+    
